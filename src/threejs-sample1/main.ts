32cb3db6bf34c87264b08b0c0408d8d52b1e56e6
Takashi Shiratori <tks.srtr89@gmail.com>
2022-08-23 01:03:16 +0900

    update threejs sample

--- src/threejs-sample1/main.ts
+++ src/threejs-sample1/main.ts
@@ -1,109 +1,69 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import vertexSource from './shader/vertex.glsl?raw'
 import fragmentSource from './shader/fragment.glsl?raw'
+import { World } from './world'
 
-let scene: THREE.Scene, camera: THREE.PerspectiveCamera, renderer: THREE.WebGLRenderer
-
-type TInRenderingTask = (time?: number) => void
-const tasksInFrame: Map<string, TInRenderingTask> = new Map()
-
-const clock = new THREE.Clock()
-
-/* rendering
+/* scene
 --------------------------------------*/
-const frame = () => {
-	const elapsedTime = clock.getElapsedTime()
-	tasksInFrame.forEach((task) => task(elapsedTime))
-	renderer.render(scene, camera)
-	requestAnimationFrame(frame)
-}
+const scene = new THREE.Scene()
 
-/* resize
+/* camera
 --------------------------------------*/
-const onResize = () => {
-	const width = window.innerWidth
-	const height = window.innerHeight
-	renderer.setPixelRatio(window.devicePixelRatio)
-	renderer.setSize(width, height)
-	camera.aspect = width / height
-	camera.updateProjectionMatrix()
-}
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.x = 50
+camera.position.y = 50
+camera.position.z = 100
+camera.lookAt(scene.position)
 
-const init = () => {
-	/* scene
-    --------------------------------------*/
-	scene = new THREE.Scene()
+/* renderer
+--------------------------------------*/
+const renderer = new THREE.WebGLRenderer()
+renderer.setClearColor(new THREE.Color(0x000000))
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-	/* camera
-    --------------------------------------*/
-	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
-	camera.position.x = 50
-	camera.position.y = 50
-	camera.position.z = 100
-	camera.lookAt(scene.position)
+/* Geometry
+--------------------------------------*/
+const boxGeometry = new THREE.BoxGeometry(40, 40, 40, 10, 10, 10)
 
-	renderer = new THREE.WebGLRenderer()
-	renderer.setClearColor(new THREE.Color(0x000000))
-	renderer.setSize(window.innerWidth, window.innerHeight)
-	document.body.appendChild(renderer.domElement)
-
-	// Geometry
-	const boxGeometry = new THREE.BoxGeometry(40, 40, 40, 10, 10, 10)
-
-	// Material
-	const shaderMaterial = new THREE.RawShaderMaterial({
-		uniforms: {
-			time: {
-				value: 0,
-			},
-			mouse: {
-				value: {
-					x: 0.0,
-					y: 0.0,
-				},
-			},
-			resolution: {
-				value: {
-					x: 0.0,
-					y: 0.0,
-				},
-			},
-			radius: {
-				value: 30.0,
+/* Material
+--------------------------------------*/
+const shaderMaterial = new THREE.RawShaderMaterial({
+	uniforms: {
+		time: {
+			value: 0,
+		},
+		mouse: {
+			value: {
+				x: 0.0,
+				y: 0.0,
 			},
 		},
-		vertexShader: vertexSource,
-		fragmentShader: fragmentSource,
-		wireframe: true,
-	})
+		resolution: {
+			value: {
+				x: 0.0,
+				y: 0.0,
+			},
+		},
+		radius: {
+			value: 30.0,
+		},
+	},
+	vertexShader: vertexSource,
+	fragmentShader: fragmentSource,
+	wireframe: true,
+})
 
-	// Mesh
-	const mesh = new THREE.Mesh(boxGeometry, shaderMaterial)
+/* Mesh
+--------------------------------------*/
+const mesh = new THREE.Mesh(boxGeometry, shaderMaterial)
+scene.add(mesh)
 
-	scene.add(mesh)
-
-	const updateGeometry: TInRenderingTask = (time) => {
-		mesh.material.uniforms.time.value = time
-	}
-
-	tasksInFrame.set('updateGeometry', updateGeometry)
-
-	/* OrbitControls
-    -------------------------------------------------------------*/
-	const orbitControls = new OrbitControls(camera, renderer.domElement)
-	orbitControls.autoRotate = false
-	orbitControls.enableDamping = true
-	orbitControls.dampingFactor = 0.12
-
-	/* resize
-    -------------------------------------------------------------*/
-	window.addEventListener('resize', onResize)
-
-	/* start animation
-    -------------------------------------------------------------*/
-	frame()
+const updateGeometry = (time?: number) => {
+	mesh.material.uniforms.time.value = time
 }
 
-init()
+const world = new World({ scene, camera, renderer })
+world.addFrameTask({ taskName: 'updateGeometry', task: updateGeometry })
+world.frame()
---
+++ src/threejs-sample1/world.ts
@@ -0,0 +1,54 @@
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as THREE from 'three'
+
+type TInRenderingTask = (time?: number) => void
+
+type WorldProps = {
+	scene: THREE.Scene
+	camera: THREE.PerspectiveCamera
+	renderer: THREE.WebGLRenderer
+}
+
+export class World {
+	scene: THREE.Scene
+	camera: THREE.PerspectiveCamera
+	renderer: THREE.WebGLRenderer
+	clock: THREE.Clock
+	tasksInFrame: Map<string, TInRenderingTask> = new Map()
+
+	constructor({ scene, camera, renderer }: WorldProps) {
+		this.scene = scene
+		this.camera = camera
+		this.renderer = renderer
+		this.clock = new THREE.Clock()
+
+		window.addEventListener('resize', this.onResize)
+
+		const orbitControls = new OrbitControls(camera, renderer.domElement)
+		orbitControls.autoRotate = false
+		orbitControls.enableDamping = true
+		orbitControls.dampingFactor = 0.12
+	}
+
+	addFrameTask = ({ taskName, task }: { taskName: string; task: TInRenderingTask }) => {
+		this.tasksInFrame.set(taskName, task)
+	}
+
+	frame = () => {
+		const { scene, camera, renderer, clock, tasksInFrame } = this
+		const elapsedTime = clock.getElapsedTime()
+		tasksInFrame.forEach((task) => task(elapsedTime))
+		renderer.render(scene, camera)
+		requestAnimationFrame(this.frame)
+	}
+
+	onResize = () => {
+		const { camera, renderer } = this
+		const width = window.innerWidth
+		const height = window.innerHeight
+		renderer.setPixelRatio(window.devicePixelRatio)
+		renderer.setSize(width, height)
+		camera.aspect = width / height
+		camera.updateProjectionMatrix()
+	}
+}
