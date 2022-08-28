592e55fb152a7ba719dd850f66ebf7f533c0bee2
Takashi Shiratori <tks.srtr89@gmail.com>
2022-08-28 17:00:20 +0900

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-const root = resolve(__dirname, 'src');
-const outDir = resolve(__dirname, 'dist');
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
 
 export default defineConfig({
-  root,
-  build: {
-    outDir,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(root, 'index.html'),
-        sample1: resolve(root, 'sample1', 'index.html'),
-      },
-    },
-  },
-});
+	root,
+	build: {
+		outDir,
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				'webgl-sample1': resolve(root, 'webgl-sample1', 'index.html'),
+				'threejs-sample1': resolve(root, 'threejs-sample1', 'index.html'),
+			},
+		},
+	},
+})
