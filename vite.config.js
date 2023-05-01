408e1f64394260cdacfe83b892e31f5d89e276ee
takashi <tks.srtr89@gmail.com>
2023-05-01 15:06:01 +0900

    fix indent

--- vite.config.js
+++ vite.config.js
@@ -12,37 +12,37 @@ console.log('pageDirNameList:', pageDirNameList)
 
 // rollupOptions用のコンフィグを作成
 const pageConfig = pageDirNameList.reduce((arr, pageName) => {
-	arr[pageName] = resolve(root, 'pages', pageName, 'index.html')
-	return arr
+  arr[pageName] = resolve(root, 'pages', pageName, 'index.html')
+  return arr
 }, {})
 
 // 各ページ遷移用リストのHTMLを作成
 const paheListHtml = pageDirNameList
-	.map((pageName) => `<li><a target="_blank" href="./pages/${pageName}/index.html">${pageName}</a></li>`)
-	.join('')
+  .map((pageName) => `<li><a target="_blank" href="./pages/${pageName}/index.html">${pageName}</a></li>`)
+  .join('')
 
 // rollupのプラグインを定義
 // index.htmlを書き換えて各ページへ遷移するリンク用HTMLを差し込む
 const htmlPlugin = () => {
-	return {
-		name: 'html-transform',
-		transformIndexHtml(html) {
-			return html.replace(/<ul id="pageIndex"><\/ul>/, `<ul id="pageIndex">${paheListHtml}</ul>`)
-		},
-	}
+  return {
+    name: 'html-transform',
+    transformIndexHtml(html) {
+      return html.replace(/<ul id="pageIndex"><\/ul>/, `<ul id="pageIndex">${paheListHtml}</ul>`)
+    },
+  }
 }
 
 // Viteのコンフィグを定義
 export default defineConfig({
-	root,
-	build: {
-		outDir,
-		rollupOptions: {
-			input: {
-				main: resolve(root, 'index.html'),
-				...pageConfig,
-			},
-		},
-	},
-	plugins: [htmlPlugin()],
+  root,
+  build: {
+    outDir,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        ...pageConfig,
+      },
+    },
+  },
+  plugins: [htmlPlugin()],
 })
