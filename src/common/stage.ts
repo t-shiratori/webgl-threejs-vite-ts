a77dd29475c89e3ed375388ebcec9460079d5dd4
takashi <tks.srtr89@gmail.com>
2023-05-01 14:49:34 +0900

    update prettier

--- src/common/stage.ts
+++ src/common/stage.ts
@@ -3,49 +3,49 @@ import * as THREE from 'three'
 type TInRenderingTask = (time?: number) => void
 
 type WorldProps = {
-	scene: THREE.Scene
-	camera: THREE.PerspectiveCamera | THREE.OrthographicCamera
-	renderer: THREE.WebGLRenderer
+  scene: THREE.Scene
+  camera: THREE.PerspectiveCamera | THREE.OrthographicCamera
+  renderer: THREE.WebGLRenderer
 }
 
 export class Stage {
-	scene: THREE.Scene
-	camera: THREE.PerspectiveCamera | THREE.OrthographicCamera
-	renderer: THREE.WebGLRenderer
-	clock: THREE.Clock
-	tasksInFrame: Map<string, TInRenderingTask> = new Map()
+  scene: THREE.Scene
+  camera: THREE.PerspectiveCamera | THREE.OrthographicCamera
+  renderer: THREE.WebGLRenderer
+  clock: THREE.Clock
+  tasksInFrame: Map<string, TInRenderingTask> = new Map()
 
-	constructor({ scene, camera, renderer }: WorldProps) {
-		this.scene = scene
-		this.camera = camera
-		this.renderer = renderer
-		this.clock = new THREE.Clock()
-		window.addEventListener('resize', this.onResize)
-	}
+  constructor({ scene, camera, renderer }: WorldProps) {
+    this.scene = scene
+    this.camera = camera
+    this.renderer = renderer
+    this.clock = new THREE.Clock()
+    window.addEventListener('resize', this.onResize)
+  }
 
-	addFrameTask = ({ taskName, task }: { taskName: string; task: TInRenderingTask }) => {
-		this.tasksInFrame.set(taskName, task)
-	}
+  addFrameTask = ({ taskName, task }: { taskName: string; task: TInRenderingTask }) => {
+    this.tasksInFrame.set(taskName, task)
+  }
 
-	frame = () => {
-		const { scene, camera, renderer, clock, tasksInFrame } = this
-		const elapsedTime = clock.getElapsedTime()
-		tasksInFrame.forEach((task) => task(elapsedTime))
-		renderer.render(scene, camera)
-		requestAnimationFrame(this.frame)
-	}
+  frame = () => {
+    const { scene, camera, renderer, clock, tasksInFrame } = this
+    const elapsedTime = clock.getElapsedTime()
+    tasksInFrame.forEach((task) => task(elapsedTime))
+    renderer.render(scene, camera)
+    requestAnimationFrame(this.frame)
+  }
 
-	start = () => {
-		this.frame()
-	}
+  start = () => {
+    this.frame()
+  }
 
-	onResize = () => {
-		const { camera, renderer } = this
-		const width = window.innerWidth
-		const height = window.innerHeight
-		renderer.setPixelRatio(window.devicePixelRatio)
-		renderer.setSize(width, height)
-		if (camera.type === 'PerspectiveCamera') camera.aspect = width / height
-		camera.updateProjectionMatrix()
-	}
+  onResize = () => {
+    const { camera, renderer } = this
+    const width = window.innerWidth
+    const height = window.innerHeight
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(width, height)
+    if (camera.type === 'PerspectiveCamera') camera.aspect = width / height
+    camera.updateProjectionMatrix()
+  }
 }
--- src/common/webgl.ts
+++ src/common/webgl.ts
@@ -1,54 +1,54 @@
 type WebGLProps = { elementId: string; vertexSource: string; fragmentSource: string }
 
 export class WebGL {
-	canvas: HTMLCanvasElement | null = null
-	gl2: WebGL2RenderingContext | null = null
-	program: WebGLProgram | null = null
+  canvas: HTMLCanvasElement | null = null
+  gl2: WebGL2RenderingContext | null = null
+  program: WebGLProgram | null = null
 
-	constructor({ elementId, vertexSource, fragmentSource }: WebGLProps) {
-		const canvas = document.getElementById(elementId) as HTMLCanvasElement
-		if (!canvas) return
-		canvas.width = window.innerWidth
-		canvas.height = window.innerHeight
+  constructor({ elementId, vertexSource, fragmentSource }: WebGLProps) {
+    const canvas = document.getElementById(elementId) as HTMLCanvasElement
+    if (!canvas) return
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
 
-		const gl2 = canvas.getContext('webgl2')
+    const gl2 = canvas.getContext('webgl2')
 
-		if (!gl2) return
+    if (!gl2) return
 
-		const vertexShader = WebGL.getShader(gl2, gl2.VERTEX_SHADER, vertexSource)
-		const fragmentShader = WebGL.getShader(gl2, gl2.FRAGMENT_SHADER, fragmentSource)
+    const vertexShader = WebGL.getShader(gl2, gl2.VERTEX_SHADER, vertexSource)
+    const fragmentShader = WebGL.getShader(gl2, gl2.FRAGMENT_SHADER, fragmentSource)
 
-		if (!vertexShader || !fragmentShader) return
+    if (!vertexShader || !fragmentShader) return
 
-		const program = gl2.createProgram()
+    const program = gl2.createProgram()
 
-		if (!program) return
+    if (!program) return
 
-		gl2.attachShader(program, vertexShader)
-		gl2.attachShader(program, fragmentShader)
-		gl2.linkProgram(program)
+    gl2.attachShader(program, vertexShader)
+    gl2.attachShader(program, fragmentShader)
+    gl2.linkProgram(program)
 
-		if (!gl2.getProgramParameter(program, gl2.LINK_STATUS)) console.error('Could not initialize shaders')
+    if (!gl2.getProgramParameter(program, gl2.LINK_STATUS)) console.error('Could not initialize shaders')
 
-		gl2.useProgram(program)
+    gl2.useProgram(program)
 
-		this.gl2 = gl2
-		this.canvas = canvas
-		this.program = program
-	}
+    this.gl2 = gl2
+    this.canvas = canvas
+    this.program = program
+  }
 
-	static getShader = (gl2: WebGL2RenderingContext, shaderType: number, shaderSource: string) => {
-		if (!gl2) return
+  static getShader = (gl2: WebGL2RenderingContext, shaderType: number, shaderSource: string) => {
+    if (!gl2) return
 
-		const shader = gl2.createShader(shaderType)
+    const shader = gl2.createShader(shaderType)
 
-		if (!shader) return
+    if (!shader) return
 
-		gl2.shaderSource(shader, shaderSource)
-		gl2.compileShader(shader)
+    gl2.shaderSource(shader, shaderSource)
+    gl2.compileShader(shader)
 
-		if (!gl2.getShaderParameter(shader, gl2.COMPILE_STATUS)) return console.error(gl2.getShaderInfoLog(shader))
+    if (!gl2.getShaderParameter(shader, gl2.COMPILE_STATUS)) return console.error(gl2.getShaderInfoLog(shader))
 
-		return shader
-	}
+    return shader
+  }
 }
